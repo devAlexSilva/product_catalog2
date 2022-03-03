9c1faa0ea2c316e4bf14cfd759c570a385362be0
Alex Silva <9apsilva@gmail.com>
2022-03-02 22:14:53 -0300

    show products by category

--- cms/query.js
+++ cms/query.js
@@ -1,14 +1,34 @@
 import Prismic from '@prismicio/client'
 
 const client = Prismic.client(process.env.NEXT_PUBLIC_API_URL);
-//const results = async()=> await client.getTags();
 
+function formatQueryResults(resultsToFormat, resultsFormated) {
+//console.log(resultsToFormat)
+    resultsToFormat.map(item => {
+        resultsFormated.push({
+            productTitle: item.tags[0],
+            productKey: item.id,
+            productImg: item.data.image.url,//.replace("&w=50&h=50", ""),
+            productName: item.data.name,
+            productPrice: item.data.price
+        })
+    })
+    return resultsFormated;
+}
 
 
 export const PrismicQuery = {
 
     getTags: async () => {
         const results = await client.getTags();
-        return results
+        return results;
+    },
+
+    getProductsByTag: async(tagName) => {
+        const { results } = await client.query(Prismic.Predicates.at('document.tags', [tagName]));
+        const resultsFormated = []
+
+        return formatQueryResults(results, resultsFormated);
+        
     }
 }
--- components/rowProducts.jsx
+++ components/rowProducts.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react'
+import { PrismicQuery } from '../cms/query'
 import styles from '../componentsStyles/RowProducts.module.css'
+
 export default function RowProducts({ category }) {
 
+    const [products, setProducts] = useState([]);
+    useEffect(async () => {
+        const results = await PrismicQuery.getProductsByTag(category);
+        setProducts(results);
+    }, [])
 
     return (
-        <div className={styles.sectionCategory}>
-            <h1>{category}</h1>
+        <div className={styles.category} >
+                <h1>{category}</h1>
+            <div className={styles.card_row}>
+ 
+
+                {
+                    products.map((item) => {
+                        return (
+                            <ul key={item.productKey} className={styles.card}>
+                                <li><img src={item.productImg} alt={item.productName}/></li>
+                                <li><p>{item.productName}</p></li>
+                                <li><p>{item.productPrice}</p></li>
+                            </ul>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
-
