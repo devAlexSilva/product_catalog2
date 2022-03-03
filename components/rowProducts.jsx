273d4d3318596aeb460a7555be087c8331267c49
Alex Silva <9apsilva@gmail.com>
2022-03-03 18:19:05 -0300

    add css in row list of products

--- components/rowProducts.jsx
+++ components/rowProducts.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { PrismicQuery } from '../cms/query'
 import styles from '../componentsStyles/RowProducts.module.css'
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material/ArrowBackIosNew';
 
 export default function RowProducts({ category }) {
 
@@ -11,23 +12,31 @@ export default function RowProducts({ category }) {
     }, [])
 
     return (
-        <div className={styles.category} >
-                <h1>{category}</h1>
-            <div className={styles.card_row}>
- 
+        <div className={styles.category}>
 
-                {
-                    products.map((item) => {
-                        return (
-                            <ul key={item.productKey} className={styles.card}>
-                                <li><img src={item.productImg} alt={item.productName}/></li>
-                                <li><p>{item.productName}</p></li>
-                                <li><p>{item.productPrice}</p></li>
-                            </ul>
-                        )
-                    })
-                }
+
+            <h1>{category}</h1>
+
+            <div className={styles.row_area}>
+                <div className={styles.row}>
+                    {
+                        products.map((item) => {
+                            return (
+
+
+                                <ul key={item.productKey} className={styles.item}>
+                                    <li><img src={item.productImg} alt={item.productName} /></li>
+                                    <li><h3>{item.productName}</h3></li>
+                                    <li><span>{item.productPrice}</span></li>
+                                </ul>
+
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
 }
+
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,32 @@
 import { PrismicQuery } from '../cms/query'
 import RowProducts from '../components/rowProducts'
+import styles from '../pagesStyles/Home.module.css'
 
 export default function Home({ responseQuery }) {
 
   return (
-    <>
+
+      <div style={{
+        height: '100vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: 'url(/night-sky.jpg)'
+      }}>
+
+      <div className={styles.faded} >
+        </div>
+
       <h1>index</h1>
 
       {
         responseQuery.map((item) => {
           return (
-          <RowProducts key={item} category={item} />
+            <RowProducts key={item} category={item} />
           )
         })
       }
 
-    </>
+    </div>
   )
 }
 
